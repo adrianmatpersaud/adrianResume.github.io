f1cd6108793e7b76ff67b0151f8050219b77b6ed
adrianpersaud <apersau@purdue.edu>
2024-03-12 15:52:42 -0400

    first push

---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "# adrianResume.github.io" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:adrianmatpersaud/adrianResume.github.io.git
+git push -u origin main
